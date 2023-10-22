64d4cabd7838e117ef2aac4491b1f76985ea707b
EmilSuuronen <emilsuuronen@gmail.com>
2023-10-22 21:59:37 +0300

    update content

--- script/backgroundAnimation.js
+++ script/backgroundAnimation.js
@@ -1,16 +1,15 @@
 const container = document.body;
 const dots = [];
 
-
 function createGridDots() {
     const dotSize = 35;
-    const numRows = (document.body.scrollHeight / 40);
+    const numRows = (document.body.scrollHeight/ 40);
     const numCols = (window.innerWidth / 40);
     const dotSpacingX = window.innerWidth / numCols;
     const dotSpacingY = document.body.scrollHeight / numRows;
 
     const fragment = document.createDocumentFragment();
-
+    
     for (let row = 0; row < numRows; row++) {
         for (let col = 0; col < numCols; col++) {
             const x = col * dotSpacingX + dotSize / 2;
@@ -20,9 +19,9 @@ function createGridDots() {
             dot.className = "grid-dot";
             dot.style.left = `${x}px`;
             dot.style.top = `${y}px`;
-            
-            dots.push(dot);
+
             fragment.appendChild(dot);
+            dots.push(dot);
         }
     }
     container.appendChild(fragment);
@@ -31,15 +30,14 @@ function createGridDots() {
 function moveDots(event) {
     dots.forEach((dot) => {
         const maxDistance = 50;
-
         const dotX = parseInt(dot.style.left);
         const dotY = parseInt(dot.style.top);
-        const dx = event.clientX - dotX;
-        const dy = event.clientY - dotY;
+        const dx = event.pageX - dotX;
+        const dy = event.pageY - dotY;
         const distance = Math.sqrt(dx * dx + dy * dy);
-        
+
         let dotSize = Math.min(maxDistance / distance, 1) * 8;
-        
+
         dot.style.width = `${dotSize}px`;
         dot.style.height = `${dotSize}px`;
     });
--- script/colorModeSelector.js
+++ script/colorModeSelector.js
@@ -1,22 +1,30 @@
-
 let isLightMode = true
 
 function changeColor() {
     const bodyColors = document.body
     const dotColors = document.querySelectorAll(".grid-dot");
-    const buttonColor = document.querySelectorAll(".colorModeButton");
+    const translucentBox = document.querySelectorAll(".translucent-box")
 
-    if (!isLightMode){
-        bodyColors.style.backgroundColor = ("#ffffff")
+    if (!isLightMode) {
+        bodyColors.style.backgroundColor = ("#f0f0f0")
+        
         dotColors.forEach(dot => {
             dot.style.backgroundColor = "#000000";
         });
+
+        translucentBox.forEach(item => {
+            item.style.backgroundColor = "rgba(0,0,0,0.8)"
+        });
         isLightMode = true
-        console.log(isLightMode);
     } else {
         bodyColors.style.backgroundColor = ("#000000")
+
         dotColors.forEach(dot => {
             dot.style.backgroundColor = "#ffffff";
+        });
+
+        translucentBox.forEach(item => {
+            item.style.backgroundColor = "rgba(49,49,49,0.5)"
         });
         isLightMode = false
     }
